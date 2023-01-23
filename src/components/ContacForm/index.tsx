3e650a52f4d238239f6f50ae34713734bd0ad10c
Anderson Rodrigues <andersonrodrigues.silva96@gmail.com>
2023-01-23 12:09:35 -0300

    refactor: implemented form validation on name and email fields

--- src/components/ContacForm/index.tsx
+++ src/components/ContacForm/index.tsx
@@ -1,32 +1,118 @@
-import FormGroup from "../FormGroup";
-import { Form, ButtonContainer } from "./styles";
+import React, { useState } from "react";
 
+import isEmailValid from "../../utils/isValidEmail";
+
+import FormGroup from "../FormGroup";
 import Input from "../Input";
 import Select from "../Select";
 import Button from "../Button";
+
+import { Form, ButtonContainer } from "./styles";
 
 interface ContactFormProps {
   buttonLabel: string;
 }
 
 export default function ContactForm({ buttonLabel }: ContactFormProps) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [category, setCategory] = useState("");
+  const [errors, setErrors] = useState(Array<Errors>);
+
+  type Errors = {
+    field?: string;
+    message?: string;
+  };
+
+  function handleNameChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setName(event.target.value);
+
+    if (!event.target.value) {
+      setErrors((prevState) => [
+        ...prevState,
+        { field: "name", message: "Nome é obrigatório" },
+      ]);
+    } else {
+      setErrors((prevState) =>
+        prevState.filter((error) => error.field !== "name")
+      );
+    }
+  }
+
+  function handleEmailChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setEmail(event.target.value);
+
+    if (event.target.value && !isEmailValid(event.target.value)) {
+      const errorAlreadyExists = errors.find(
+        (error) => error.field === "email"
+      );
+
+      if (errorAlreadyExists) {
+        return;
+      }
+
+      setErrors((prevState) => [
+        ...prevState,
+        { field: "email", message: "Digite um e-mail válido" },
+      ]);
+    } else {
+      setErrors((prevState) =>
+        prevState.filter((error) => error.field !== "email")
+      );
+    }
+  }
+
+  function getErrorMessageByFieldName(fieldName: string) {
+    return errors.find((error) => error.field === fieldName)?.message;
+  }
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log({
+      name,
+      email,
+      phone,
+      category,
+    });
+  }
+
   return (
-    <Form>
-      <FormGroup>
-        <Input placeholder="nome" />
+    <Form onSubmit={handleSubmit}>
+      <FormGroup error={getErrorMessageByFieldName("name")}>
+        <Input
+          placeholder="Nome"
+          value={name}
+          onChange={handleNameChange}
+          error={getErrorMessageByFieldName("name") ? true : false}
+        />
       </FormGroup>
 
-      <FormGroup error="O formato do e-mail é inválido.">
-        <Input placeholder="email" error />
+      <FormGroup error={getErrorMessageByFieldName("email")}>
+        <Input
+          placeholder="Email"
+          value={email}
+          onChange={handleEmailChange}
+          error={getErrorMessageByFieldName("email") ? true : false}
+        />
       </FormGroup>
 
       <FormGroup>
-        <Input placeholder="telefone" />
+        <Input
+          placeholder="Telefone"
+          value={phone}
+          onChange={(event) => setPhone(event.target.value)}
+        />
       </FormGroup>
 
       <FormGroup>
-        <Select>
+        <Select
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        >
+          <option value="">Categoria</option>
           <option value="instagram">Instagram</option>
+          <option value="discord">Discord</option>
         </Select>
       </FormGroup>
 
