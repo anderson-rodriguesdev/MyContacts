daf3c800639bdad42bbc33ad1c0e289809d4b3b3
Anderson Rodrigues <andersonrodrigues.silva96@gmail.com>
2023-02-09 08:01:32 -0300

    refactor: using useCallback to prevent loop inside useEffect

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState, useCallback } from "react";
 import { Link } from "react-router-dom";
 
 import {
@@ -35,7 +35,7 @@ export default function Home() {
     );
   }, [contacts, searchTerm]);
 
-  async function loadContacts() {
+  const loadContacts = useCallback(async () => {
     try {
       setIsLoading(true);
       const contacsList = await ContactsService.listContacts(orderBy);
@@ -47,11 +47,11 @@ export default function Home() {
     } finally {
       setIsLoading(false);
     }
-  }
+  }, [orderBy]);
 
   useEffect(() => {
     loadContacts();
-  }, [orderBy]);
+  }, [loadContacts]);
 
   function handleToggleOrderBy() {
     setOrderBy((prevState) => (prevState === "asc" ? "desc" : "asc"));
